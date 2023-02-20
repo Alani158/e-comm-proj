dd4969d4c9ab41ede2951d4d234b3470b450a21e
Alani Mujeeb <109475364+Alani158@users.noreply.github.com>
2023-02-20 19:31:59 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import Home from "./Home";
+import Navbar from "./navbar";
+import "./App.css"
+import Electronic from "./electronic";
+import Jewelry from "./jewelry";
+import Footer from "./footer";
+
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <Navbar/>  
+      <Home/>
+   <Electronic/>
+<Jewelry/>
+ <Footer/>
+
+    </div> 
+
+    
+  );
+}
+
+export default App;
